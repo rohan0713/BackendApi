d7a12297685d11a315baa001d3c89b18fe924e6a
RohanRamola <rohanramola63@gmail.com>
2023-11-23 04:38:37 +0530

    reverted changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, url_for
 from mongoConnection import client
 
 app = Flask(__name__)
@@ -8,18 +8,73 @@ def apiStatus():
     result = "Hi! Welcome to Clean hires backend! Use the data carefully"
     return jsonify(result)
 
-@app.route('/authenticate/<string:email>/<string:password>', methods=['GET'])
-def authenticateUser(email, password):
+@app.route('/authenticate', methods=['GET'])
+def authenticateUser():
     
     dbs = client.cleanHires
     collection = dbs.users
+
+    email = request.args.get('email')
+    password = request.args.get('password')
+    print(email + " " + password)
     query = {"email": email, "password": password}
 
     result = collection.find_one(query)
     if result is None:
-        return jsonify("No users found")
+        data = {"status" : False, "message": "user not found"}
+        return jsonify(data)
     else :
-        return jsonify("user found")
+        data = {"status" : True, "message": "user found"}
+        return jsonify(data)
+
+@app.route("/create", methods=['POST'])
+def createUser():
+    
+    dbs = client.cleanHires
+    collection = dbs.users
+
+    email = request.args.get('email')
+    password = request.args.get('password')
+    print(email)
+    print(password)
+    
+    if(email is None or password is None):
+        return jsonify('Add valid query params in url')
+    elif (len(email) <= 0 or len(password) <= 0):
+        return jsonify('Invalid data')
+
+    query = {
+        "email": email, "password": password
+    }
+
+    collection.insert_one(query)
+    data = {"status" : True}
+    return jsonify(data)
+
+@app.route("/update", methods=['PUT'])
+def updatePassword():
+
+    dbs = client.cleanHires
+    collection = dbs.users
+
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    newPassword = request.args.get('new')
+
+    query = {
+        "email": email, "password": password
+    }
+
+    value = {"$set" : {
+        "password" : newPassword
+    }}
+
+    response = collection.update_one(query, value)
+    if response.matched_count == 0:
+        return 'No data found', 404
+    else:
+        return 'Email updated successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
